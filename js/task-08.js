cc895dca96a3fc560ba0eb134583fa51cb30c5cb
NataliaMazurak <mazurak25@gmail.com>
2023-06-30 08:56:31 +0300

    changes in the 8 tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,25 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 console.log(form);
+
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    
-    const {
-      elements: { email, password }
-    } = event.currentTarget;
-  
-    if (email.value === "" || password.value === "") {
-      return alert("Please fill in all the fields!");
-    }
-  
-    console.log(`Login: ${email.value}, Password: ${password.value}`);
-    event.currentTarget.reset();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
   }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
